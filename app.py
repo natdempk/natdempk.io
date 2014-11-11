3f62131bc0481800817743fabeb1a6a81e1cf717
Nat Dempkowski <natdempk@gmail.com>
2014-11-11 16:36:28 -0500

    Fix static URLs

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "natdempk.io"
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
