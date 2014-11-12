d09f584677e9d02a07366adb698595ca8650bc88
Nat Dempkowski <natdempk@gmail.com>
2014-11-11 19:43:22 -0500

    Get route sources and syntax highlight them

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+import pprint
+import inspect
+import sys
 app = Flask(__name__)
+
+@app.route("/foo")
+def foo():
+    return "foo"
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    routes = [key for key in app.url_map._rules_by_endpoint if key != 'static']
+
+    sources = [inspect.getsource(getattr(sys.modules[__name__], method)) for method in routes]
+    highlights = [highlight(s, PythonLexer(), HtmlFormatter(noclasses=True)) for s in sources]
+
+    return render_template('index.html', routes=highlights)
 
 @app.errorhandler(404)
 def page_not_found(e):
